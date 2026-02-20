740207f3693a92022732e4032df0be0c727b476d
Jaime Rosales <jamitus21@gmail.com>
2026-02-20 16:49:43 +0000

    test: add euroToDollar tests (failing)

--- SD2_LabTwo_3142108/src/griffith/ConversionsTest.java
+++ SD2_LabTwo_3142108/src/griffith/ConversionsTest.java
@@ -1,5 +1,19 @@
+// Name: Jaime Rosales
+// Student Number: 3142108
+
 package griffith;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class ConversionsTest {
 
+    @Test
+    public void testEuroToDollar() {
+        Conversions c = new Conversions();
+
+        assertEquals(110.0, c.euroToDollar(100.0), 0.0001);
+        assertEquals(0.0, c.euroToDollar(0.0), 0.0001);
+        assertEquals(-55.0, c.euroToDollar(-50.0), 0.0001);
+    }
 }
